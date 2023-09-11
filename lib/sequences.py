124af8cae4cdaf326ce1c42f086e1e7dae53c360
B-atuti <brian.atuti@student.moringaschool.com>
2023-09-11 12:32:28 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,21 +2,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    fibonacci_sequence = []
-    
-    if length > 0:
-        fibonacci_sequence.append(0)
-        
-        if length > 1:
-            fibonacci_sequence.append(1)
-            
-            if length > 2:
-                for i in range(2, length):
-                    fibonacci_sequence.append(
-                        fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
-                
-    return fibonacci_sequence
+   fibonacci_arr = list()
+   if length == 0:
+      print( fibonacci_arr)
+   elif length == 1:
+      fibonacci_arr.append(0)
+      print(fibonacci_arr)
+   elif length == 2:
+      fibonacci_arr.extend([0,1])
+      print(fibonacci_arr)
+   elif length > 2:
+      fibonacci_arr.extend([0,1])
+      for i in range(length-2):
+         fibonacci_arr.append(fibonacci_arr[-1] + fibonacci_arr[-2])
+      print(fibonacci_arr)
 
-length = int(input("Enter the length of Fibonacci sequence: "))
-fibonacci_sequence = print_fibonacci(length)
-print(fibonacci_sequence)
+print_fibonacci(10)
